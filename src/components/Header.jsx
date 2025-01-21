1d89ba187c96fe22493d1edb3ce3919c9309e93c
Roshan Gupta <lucysigma72@gmail.com>
2025-01-21 19:00:41 +0545

    create header component with UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function Header() {
   return (
-    <header>
-      Header
+    <header className="bg-gradient-to-r from-blue-300 to to-purple-400 shadow-lg">
+        <div className="max-w-6xl mx-auto flex justify-between items-center p-3">
+            {/* logo */}
+            <Link href='/' className="text-2xl font-extrabold group cursor-pointer">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-lg group-hover:from-blue-400 group-hover:to-blue-600">
+                Auth
+            </span>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700 group-hover:from-purple-400 group-hover:to-purple-600">
+                App
+            </span>
+            </Link>
+            {/* add a navigation menu */}
+            <nav>
+                <ul className="flex gap-4">
+                    <Link href="/">Home</Link>
+                    <Link href="/about">About</Link>
+                    <Link href="/sign-in">Sign in</Link>
+                </ul>
+            </nav>
+        </div>
     </header>
   )
 }
